2eef0dcf904bf0100397cbdeb550a16112f96024
tiredofchoosing <isewum@gmail.com>
2024-02-21 23:59:58 +0300

    added globalization

--- app/controllers.js
+++ app/controllers.js
@@ -1,33 +1,52 @@
 import fetch from 'node-fetch'
 import config from 'config'
-import {classes, roles} from './data.js'
-import dungeonsInfo from './../static/json/DungeonsInfo.json' assert { type: 'json' }
-import battlegroundsInfo from './../static/json/BattlegroundsInfo.json' assert { type: 'json' }
+import {classes, roles, dungeons_info, battlegrounds_info, globalization} from './data.js'
 
-const lang = 'ru'
+const lang_default = 'ru'
+function changeLang(req) {
+    let cookieLangArr = req.headers.cookie?.split(';').find(c => c.startsWith('lang='))
+    if (cookieLangArr === undefined)
+        return lang_default
+
+    let cookieLang = cookieLangArr.split('=')[1]
+    if (cookieLang === 'en') {
+        return 'en'
+    } else if (cookieLang === 'ru') {
+        return 'ru'
+    } else {
+        return lang_default
+    }
+}
+
 export async function dungeons(req, res) {
+    const lang = changeLang(req)
+
     const response = await fetch(config.get('dungeons'));
     if (!response)
         return res.type('txt').send('error')
 
     res.render('dungeons', {
         data: await response.json(),
-        dungeonsInfo,
-        classes,
+        dungeons_info: dungeons_info[lang],
+        strings: globalization[lang],
+        classes: classes[lang],
         roles,
         lang
     })
 }
 
 export async function battlegrounds(req, res) {
+    const lang = changeLang(req)
+
     const response = await fetch(config.get('battlegrounds'));
     if (!response)
         return res.type('txt').send('error')
 
     res.render('battlegrounds', {
         data: await response.json(),
-        battlegroundsInfo,
-        classes,
+        battlegrounds_info: battlegrounds_info[lang],
+        strings: globalization[lang],
+        classes: classes[lang],
         roles,
         lang
     })
--- app/data.js
+++ app/data.js
@@ -1,3 +1,14 @@
+import dg_ru from './../static/json/DungeonsInfo_ru.json' assert { type: 'json' }
+import dg_en from './../static/json/DungeonsInfo_en.json' assert { type: 'json' }
+import bg_ru from './../static/json/BattlegroundsInfo_ru.json' assert { type: 'json' }
+import bg_en from './../static/json/BattlegroundsInfo_en.json' assert { type: 'json' }
+import str_ru from './../static/json/strings_ru.json' assert { type: 'json' }
+import str_en from './../static/json/strings_en.json' assert { type: 'json' }
+
+export const dungeons_info = { ru: dg_ru, en: dg_en }
+export const battlegrounds_info = { ru: bg_ru, en: bg_en }
+export const globalization = { ru: str_ru, en: str_en }
+
 export const classes = {
     ru: ['Воин', 'Рыцарь', 'Убийца', 'Берсерк', 'Маг', 'Лучник', 'Жрец', 'Мистик', 'Жнец', 'Инженер', 'Крушитель', 'Шиноби', 'Валькирия'],
     en: ['Warrior', 'Lancer', 'Slayer', 'Berserk', 'Sorcerer', 'Archer', 'Priest', 'Mystic', 'Reaper', 'Gunner', 'Brawler', 'Ninja', 'Valkyrie']
