76727f5437d98d39cb143834ca188db8041fc13e
tiredofchoosing <isewum@gmail.com>
2024-05-26 00:30:47 +0300

    roles icons moved to css
    added swittch to classic icons

--- app/data.js
+++ app/data.js
@@ -13,4 +13,4 @@ export const classes = {
     ru: ['Воин', 'Рыцарь', 'Убийца', 'Берсерк', 'Маг', 'Лучник', 'Жрец', 'Мистик', 'Жнец', 'Инженер', 'Крушитель', 'Шиноби', 'Валькирия'],
     en: ['Warrior', 'Lancer', 'Slayer', 'Berserk', 'Sorcerer', 'Archer', 'Priest', 'Mystic', 'Reaper', 'Gunner', 'Brawler', 'Ninja', 'Valkyrie']
 }
-export const roles = ['shield', 'sword', 'heal']
+export const roles = ['tank', 'dmg', 'heal']
