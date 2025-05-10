40a08a1be5644a834606ac0c0de5dff1cbfd6c1b
tiredofchoosing <isewum@gmail.com>
2025-05-10 23:51:54 +0300

    added feature to keep details open

--- static/js/lfg.js
+++ static/js/lfg.js
@@ -4,20 +4,43 @@
     const content = document.getElementById('content');
     const checkboxes = [autoupdateCheckbox];
 
+    let lfgList, lfgs;
+    let openedLfgs = [];
+
+    initVariables();
+
     function initVariables() {
+        lfgList = document.getElementById('lfgList');
+        lfgs = Array.from(lfgList.getElementsByClassName('lfg-details'));
+    }
+
+    function lfgDetailsToggleHandler() {
+        const lfgId = this.dataset.lfgId;
+        const index = openedLfgs.indexOf(lfgId);
+        if (this.open && index === -1) {
+            openedLfgs.push(lfgId);
+        }
+        else if (!this.open && index !== -1) {
+            openedLfgs.splice(index, 1);
+        }
     }
 
     function refresh() {
         initVariables();
         loadState();
+        
+        lfgs.forEach(l => l.addEventListener('toggle', lfgDetailsToggleHandler));
     }
 
     function loadState() {
+        lfgs.forEach(l => l.open = openedLfgs.indexOf(l.dataset.lfgId) !== -1);
+        openedLfgs = openedLfgs.filter(o => lfgs.some(l => l.dataset.lfgId === o));
     }
 
     // register event handlers
     autoupdateCheckbox.addEventListener('change', setAutoupdate);
     content.addEventListener('contentUpdated', refresh);
+    lfgs.forEach(l => l.addEventListener('toggle', lfgDetailsToggleHandler));
 
     // restore session data
     checkboxes.forEach(e => {
