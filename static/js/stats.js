e1fdded242ffdbd88ba9d58bda362bf10371ada1
tiredofchoosing <isewum@gmail.com>
2025-05-11 19:44:57 +0300

    added stats by guild

--- static/js/stats.js
+++ static/js/stats.js
@@ -1,9 +1,12 @@
 (function() {
 
-    const canvas = document.getElementById('chart');
-	const ctx = canvas.getContext('2d');
+    const canvas1 = document.getElementById('chart_class');
+    const canvas2 = document.getElementById('chart_guild');
+	const ctx1 = canvas1.getContext('2d');
+	const ctx2 = canvas2.getContext('2d');
     const autoupdateCheckbox = document.getElementById('autoupdateCheck');
     const checkboxes = [autoupdateCheckbox];
+	const charts = [[ctx1, canvas1], [ctx2, canvas2]];
 
 	// TODO: move to CSS?
 	const fontSize = 16;
@@ -13,62 +16,71 @@
 	const gridColor = 'rgba(255, 255, 255, 0.1)';
 	const barBgColor = 'rgba(86, 136, 92, 0.5)';
 	const barBorderColor = 'rgba(255, 255, 255, 0.3)';
-	const title = canvas.dataset.title;
-	const labels = canvas.dataset.labels.split(',');
-	const values = canvas.dataset.values.split(',');
 
 	Chart.defaults.color = fontColor;
 	Chart.defaults.font.size = fontSize;
 	Chart.defaults.font.family = fontFamily;
 
 	document.fonts.ready.then(function() {
-		new Chart(ctx, {
-			type: 'bar',
-			data: {
-				labels: labels,
-				datasets: [{
-					label: title,
-					backgroundColor: barBgColor,
-					borderColor: barBorderColor,
-					borderWidth: 1,
-					data: values
-				}]
-			},
-			options: {
-				indexAxis: 'y',
-				maintainAspectRatio: false,
-				scales: {
-					x: {
-						beginAtZero: true,
-						grid: {
-							color: gridColor
+		charts.forEach(c => {
+			const [ctx, canvas] = c;
+			new Chart(ctx, {
+				type: 'bar',
+				data: {
+					labels: canvas.dataset.labels.split(','),
+					datasets: [{
+						label: canvas.dataset.title,
+						backgroundColor: barBgColor,
+						borderColor: barBorderColor,
+						borderWidth: 1,
+						data: canvas.dataset.values.split(',')
+					}]
+				},
+				options: {
+					indexAxis: 'y',
+					maintainAspectRatio: false,
+					scales: {
+						x: {
+							beginAtZero: true,
+							grid: {
+								color: gridColor
+							},
+							border: {
+								color: axisColor
+							},
+							ticks: {
+								precision: 0,
+								stepSize: 1
+							}
 						},
-						border: {
-							color: axisColor
-						},
-						ticks: {
-							precision: 0,
-							stepSize: 1
+						y: {
+							grid: {
+								color: gridColor
+							},
+							border: {
+								color: axisColor
+							},
+							ticks: {
+								callback: function (value) {
+									const lbl = this.getLabelForValue(value);
+									if (typeof lbl === 'string' && lbl.length > 14) {
+										return `${lbl.substring(0, 14)}...`;
+									}
+									return lbl;
+								},
+							},
 						}
 					},
-					y: {
-						grid: {
-							color: gridColor
-						},
-						border: {
-							color: axisColor
-						}
-					}
-				},
-				animation: {
-					duration: 0
-				},
-				plugins: {
-					legend: {
-						display: false
+					animation: {
+						duration: 0
 					},
-				},
-			}
+					plugins: {
+						legend: {
+							display: false
+						},
+					},
+				}
+			});
 		});
 	});
 
