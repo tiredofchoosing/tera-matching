45ff64957e7be3ea5b60e67a8f91cd852da907bc
tiredofchoosing <isewum@gmail.com>
2025-06-04 15:39:38 +0300

    minor fix

--- app/controllers.js
+++ app/controllers.js
@@ -11,10 +11,8 @@ function changeLang(req) {
     const clientAcceptLang = req.headers['accept-language']?.split(',')[0].split('-')[0];
     const lang = req.cookies.lang ?? clientAcceptLang;
 
-    if (lang === 'en') {
-        return 'en';
-    } else if (lang === 'ru') {
-        return 'ru';
+    if (lang === 'en' || lang === 'ru') {
+        return lang;
     } else {
         return lang_default;
     }
